d4bbf95ef33c654367530d69ea5e18d656e3a591
Ahmet Furkan Kavraz <ahmetfurkankavraz@hotmail.com>
2023-09-09 16:04:16 +0300

    Update Basket.java

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,46 @@
 package com.trendyol.shipment;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Basket {
 
+    public static final int BASKET_THRESHOLD_COUNT = 3;
+    public static final int BASKET_EMPTY_COUNT = 0;
+
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+
+        ShipmentSize selectedShipmentSize = null;
+
+        for (var shipmentSize : ShipmentSize.values()) {
+            long shipmentSizeCount = products.stream().filter(
+                    (product -> product.getSize() == shipmentSize)).count();
+
+            if (shipmentSizeCount == BASKET_EMPTY_COUNT)
+                continue;
+
+            selectedShipmentSize = shipmentSize;
+
+            if (shipmentSizeCount < BASKET_THRESHOLD_COUNT)
+                continue;
+
+            switch (shipmentSize) {
+                case SMALL:
+                    selectedShipmentSize = ShipmentSize.MEDIUM;
+                    break;
+                case MEDIUM:
+                    selectedShipmentSize = ShipmentSize.LARGE;
+                    break;
+                case LARGE:
+                    selectedShipmentSize = ShipmentSize.X_LARGE;
+                    break;
+            }
+            return selectedShipmentSize;
+        }
+
+        return selectedShipmentSize;
     }
 
     public List<Product> getProducts() {
